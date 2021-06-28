801a62df8f5f0f57b1c5baf8e677d1dba626a1c0
Dante-ELeuterio <danteeleuterio00@gmail.com>
2021-06-28 19:06:01 -0300

    ADICIONA QUIQCKSORT.C E MODIFICA PARTICIONA.C(SÓ FUNCIONA COM V[B]

--- particiona.c
+++ particiona.c
@@ -6,7 +6,7 @@
 /* v[i] <= x, para todo a <= i <=m */
 /* x < v[i], para todo m < i <=b */
 
-int particiona(vetor v,int a,int b,int x){
+int particiona(int v[],int a,int b,int x){
     int m:a-1;
 
     for (int i = a; i <= b; ++i)
--- quicksort.c
+++ quicksort.c
@@ -3,7 +3,11 @@
 /* -------------------------------------------------------------------------- */
 /* ordena v[a..b] usando o algoritmo QuickSort e devolve v */
 
-int *quicksort(int v[], int a, int b) {
-  
-  return v;
+int *quicksort(int v[], int a, int b){
+	int m;
+	if (a>=b)
+		return v;
+	m=particiona(v,a,b,v[b]);
+	quicksort(v,a,m-1);
+	quicksort(v,m+1,b);	
 }
