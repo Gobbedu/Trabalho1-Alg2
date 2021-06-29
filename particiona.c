c795eca815002209766c958d97d4ebc8a494242e
Dante-ELeuterio <danteeleuterio00@gmail.com>
2021-06-29 09:24:20 -0300

    funções de mediana

--- particiona.c
+++ particiona.c
@@ -6,14 +6,14 @@
 /* v[i] <= x, para todo a <= i <=m */
 /* x < v[i], para todo m < i <=b */
 
-int particiona(int v[],int a,int b,int x){
+iint particiona(int v[],int a,int b,int x){
     int m=a-1;
 
     for (int i = a; i <= b; ++i)
-        if v[i]<=x
-        {
+        if (v[i]<=x){
             m++;
-            troca(v,m,i)
+	    if(m != i)
+                troca(v,m,i);
         }
     return m;
 }
--- quicksort-mediana.c
+++ quicksort-mediana.c
@@ -30,14 +30,23 @@ static int mediana(int a, int b, int c) {
 /* ordena v[a..b]  usando o algoritmo  "QuickSort com mediana de  3" e
    devolve v                                                                  */
 
-int *quicksort_mediana(int v[], int a, int b) {
+int *quicksort_mediana(vetor v, int a, int b){
 	int m;
+	int x;
 	if (a>=b)
 		return v;
-	m=particiona(v,a,b,mediana(v[sorteia(a,b)],v[sorteia(a,b)],v[sorteia(a,b)]));
-	quicksort_mediana(v,a,m-1);
-	quicksort_mediana(v,m+1,b);	 
-  return v;
+	x=mediana(v[sorteia(a,b)],v[sorteia(a,b)],v[sorteia(a,b)]);
+	for (int i = a; i <=b; ++i)
+	{
+		if (v[i]==x)
+		{
+			Troca(v,i,b);
+			break;
+		}
+	}
+	m=particiona(v,a,b,v[b]);
+	quicksort(v,a,m-1);
+	quicksort(v,m+1,b);
 }
 
 
