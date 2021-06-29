602134b1ef5e5c9eb9a0daeed323dd35bc946466
gobboE <gobbolegal@gmail.com>
2021-06-28 22:51:45 -0300

    insercao e quick_insercao implementados

--- insercao.h
+++ insercao.h
@@ -1,2 +1,3 @@
 int *insercao(int v[], unsigned int a, unsigned int b);
+int *insere(int v[], int a, int b);
 int BuscaSimples(int x, int v[], int a, int b);
--- quicksort-insercao.c
+++ quicksort-insercao.c
@@ -3,9 +3,25 @@
 
 
 /* -------------------------------------------------------------------------- */
-/* ordena v[a..b] usando o algoritmo QuickSort com inserção e devolve v       */
+/* ordena v[a..b] usando o algoritmo QuickSort com inserï¿½ï¿½o e devolve v       */
 
 int *quicksort_insercao(int v[], int a, int b, unsigned int m) {
+  int aux; 
+
+  /* BASE */
+	if (a>=b)
+		return v;
+
+	aux = particiona(v,a,b,v[b]); /* ==> indice do pivo no vetor */
+
+  /* === se tamanho particao <= m, usar insertion sort ==== */
+  /* tamanho particao na esquerda eh (aux-1)-a +1 */
+  if( aux-a <= m ){ insercao(v, a, aux-1); } 
+  else { quicksort_insercao(v,a,aux-1, m); }
+
+  /* tamanho particao na direita eh b-(aux+1) +1 */
+  if( b-aux <= m ){ insercao(v, aux+1, b); }
+  else { quicksort_insercao(v,aux+1,b, m);   }
   
   return v;
 }
